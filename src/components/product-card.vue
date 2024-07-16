1394fa98e81aeb7091d1e7b6df3a96f21e2a539c
LIARA PEREIRA DUARTE <987.liara@gmail.com>
2024-07-16 15:02:55 -0300

    melhora card de produtos

--- src/components/product-card.vue
+++ src/components/product-card.vue
@@ -1,40 +1,117 @@
 <template>
-  <a
-    class="c-product-card"
-    :href="href"
-  >
-    <img
-      class="c-product-card__image"
-      :src="imageUrl"
-    />
+  <a class="c-product-card" :href="href">
+    <img class="c-product-card__image" :src="imageUrl" />
+    <span class="c-product-card__percentage" v-if="sale">
+      <em> {{ salePercentage }}% off </em>
+    </span>
+    <span class="c-product-card__price" v-if="sale">
+      <em>R$ {{ sale }}</em> <del>R$ {{ price }}</del>
+    </span>
+    <span class="c-product-card__price" v-else> R$ {{ price }} </span>
   </a>
 </template>
 
 <script setup>
-  import { computed } from 'vue'
-  import { getImageUrl, IMAGE_PRESETS } from '@/tools/get-image-url'
+import { computed } from "vue";
+import { getImageUrl, IMAGE_PRESETS } from "@/tools/get-image-url";
 
-  const props = defineProps({
-    imageId: {
-      type: String,
-      required: true
-    },
-    title: {
-      type: String,
-      required: true
-    },
-    path: {
-      type: String,
-      required: true
-    }
-  })
+const props = defineProps({
+  imageId: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  path: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  sale: {
+    type: Number,
+    required: false,
+  },
+});
 
-  const imageUrl = computed(() => getImageUrl(props.imageId, IMAGE_PRESETS.product.card))
-  const href = computed(() => `https://www.enjoei.com.br/p/${props.path}`)
+const imageUrl = computed(() =>
+  getImageUrl(props.imageId, IMAGE_PRESETS.product.card)
+);
+const href = computed(() => `https://www.enjoei.com.br/p/${props.path}`);
+const salePercentage = discountCalculator(props.sale, props.price);
+
+function discountCalculator(sale, price) {
+  const discount = price - sale;
+  return computed(() => Math.round((discount / price) * 100));
+}
 </script>
 
 <style scoped lang="scss">
-  .c-product-card {
-    display: block;
+$layout-breakpoint-desktop: 1024px;
+
+.c-product-card {
+  display: block;
+  position: relative;
+  width: 163px;
+  height: 163px;
+
+  &__image {
+    width: 100%;
   }
+
+  &__percentage {
+    position: absolute;
+    padding: 6px 5px 4px 5px;
+    border-radius: 2px;
+    background-color: var(--color-highlight);
+    right: 2px;
+    top: 2px;
+
+    em {
+      color: var(--color-white);
+      font-family: var(--font-family);
+      font-style: normal;
+      font-size: 12px;
+      line-height: 14.06px;
+    }
+  }
+
+  &__price {
+    position: absolute;
+    width: max-content;
+    padding: 6px 5px 4px 5px;
+    border-radius: 2px;
+    background-color: var(--color-white);
+    left: 2px;
+    bottom: 2px;
+
+    font-size: 12px;
+    font-weight: var(--font-weight-semi-bold);
+    font-family: var(--font-family);
+    line-height: 12px;
+
+    color: var(--color-gray-5);
+
+    // &__sale {
+    em {
+      color: var(--color-highlight);
+      font-style: normal;
+    }
+
+    del {
+      font-weight: var(--font-weight-regular);
+      color: var(--color-gray-4);
+    }
+    // }
+  }
+
+  @media (min-width: $layout-breakpoint-desktop) {
+    width: 216px;
+    height: 216px;
+  }
+}
 </style>
